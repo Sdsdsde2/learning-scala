e75e5369a130a5fe6e24d1ee56f3cbfa22d0ec48
Sdsdsde2 <imspammails31@gmail.com>
2021-09-20 17:07:51 -0500

    added comments, variables, and if statements.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,25 @@
 object Main extends App {
   println("Hello, World!")
+
+  // This is a comment in Scala
+
+  // These are Scala variables
+  val a = 1
+  var b = 2
+  // val is used for declaring an immutable variable
+  // var is used for declaring a mutable variable
+
+  // In Slaca you can declair variables without declaring their type like so
+  val s = "This is a string that I didn't have to say was a string"
+  val p = new Person("Shane")
+
+  // If statements in Scala
+  if (a == 2) {
+      // Do something
+  } else if (b == 1) {
+      // Do something
+  }
+    else {
+      // Do something if none of the if statements are true
+  }
 }
